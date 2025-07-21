db26bfda92a54f5d8857d39fd87e3a4666da95b1
Pakornkiet250 <66050250@kmitl.ac.th>
2025-07-21 11:39:09 +0700

    7

---
+++ Week3/src/example/myapp/AquariumFish.kt
@@ -0,0 +1,26 @@
+package example.myapp
+
+interface FishAction {
+    fun eat()
+}
+
+interface FishColor {
+    val color: String
+}
+
+class Plecostomus: FishAction, FishColor {
+    override val color = "gold"
+    override fun eat() {
+        println("eat algae")
+    }
+}
+
+class Shark: FishAction, FishColor {
+    override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+    object GoldColor : FishColor {
+        override val color = "gold"
+    }
+}
