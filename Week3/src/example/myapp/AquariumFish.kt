20a4b397962c0fdab01d0d0fa04fac468602764c
Pakornkiet250 <66050250@kmitl.ac.th>
2025-08-08 12:08:38 +0700

    Week3_edit

--- Week3/src/example/myapp/AquariumFish.kt
+++ Week3/src/example/myapp/AquariumFish.kt
@@ -1,18 +1,8 @@
 package example.myapp
 
-interface FishAction {
-    fun eat()
-}
-
-interface FishColor {
-    val color: String
-}
-
-class Plecostomus: FishAction, FishColor {
-    override val color = "gold"
-    override fun eat() {
-        println("eat algae")
-    }
+abstract class AquariumFish : FishAction {
+    abstract val color: String
+    override fun eat() = println("yum")
 }
 
 class Shark: FishAction, FishColor {
@@ -20,7 +10,36 @@ class Shark: FishAction, FishColor {
     override fun eat() {
         println("hunt and eat fish")
     }
-    object GoldColor : FishColor {
-        override val color = "gold"
+}
+
+//class Plecostomus: FishAction, FishColor by GoldColor {
+////    override val color = "gold" override at goldcolor
+//    override fun eat() {
+//        println("eat algae")
+//    }
+//}
+
+class Plecostomus(fishColor: FishColor = GoldColor):  FishAction by PrintingFishAction("eat algae"),
+    FishColor by GoldColor {
+    override fun eat() {
+        println("eat algae")
+    }
+}
+
+interface FishAction  {
+    fun eat()
+}
+
+interface FishColor {
+    val color: String
+}
+
+object GoldColor : FishColor {
+    override val color = "gold"
+}
+
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
     }
 }
--- Week3/src/example/myapp/Main.kt
+++ Week3/src/example/myapp/Main.kt
@@ -1,25 +1,25 @@
 package example.myapp
 
 fun buildAquarium() {
-    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
-    aquarium6.printSize()
-    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
-    myAquarium.printSize()
+    val aquarium1 = Aquarium(29)
+    aquarium1.printSize()
+    aquarium1.volume = 70
     val myTower = TowerTank(diameter = 25, height = 40)
     myTower.printSize()
 }
-fun makeFish() {
-    val shark = Shark()
-    val pleco = Plecostomus()
-    println("Shark: ${shark.color}")
-    shark.eat()
-    println("Plecostomus: ${pleco.color}")
-    pleco.eat()
-}
 
 fun main() {
-    // buildAquarium()
+    buildAquarium()
     makeFish()
 }
 
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
 
+    println("Shark: ${shark.color}")
+    shark.eat()
+
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
+}
